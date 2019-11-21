4dbc2d2be4321a36c4286b797105826b7b72a2e6
MarcoD1337 <40003501+MarcoD1337@users.noreply.github.com>
2019-11-20 21:03:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const fetch = require("node-fetch")
 const open = require("opn")
+const readline = require("readline")
 
-function look() {
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+async function look(kws) {
 fetch("https://www.supremenewyork.com/shop.json")
     .then(res => res.json())
     .then(res => {
         const arr = res.products_and_categories.new
         arr.forEach(element => {
-            if (element.name.toLowerCase().includes("dvd")) {
+            if (element.name.toLowerCase().includes(kws)) {
                 console.log("found")
                 open(`https://supremenewyork.com/shop/${element.id}`, {app: "chrome"})
             }
@@ -15,4 +21,15 @@ fetch("https://www.supremenewyork.com/shop.json")
     })
 }
 
-look()
+async function init() {
+    const kw = await prompt("What Keyword Would You Like To Use? ")
+    look(kw)
+}
+
+function prompt(q) {
+    return new Promise(resolve => {
+        rl.question(q, resolve)
+    })
+}
+
+init()
